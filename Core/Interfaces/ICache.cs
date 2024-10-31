28470b9969793566e3dca412184e0a4810f54706
Алексей <97665299+0ne290@users.noreply.github.com>
2024-10-31 23:14:07 +0800

    Update ICache.cs

--- Core/Interfaces/ICache.cs
+++ Core/Interfaces/ICache.cs
@@ -3,4 +3,6 @@ namespace Core.Interfaces;
 public interface ICache
 {
     bool TryGetTranslation(string text, string language, out string? translation);
+
+    void SetTranslation(string text, string language, string translation);
 }
