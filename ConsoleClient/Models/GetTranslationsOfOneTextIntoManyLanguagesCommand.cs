62aff9e5561699e55f2781b4a582965f48ae954f
Алексей <97665299+0ne290@users.noreply.github.com>
2024-11-02 22:18:23 +0800

    Update GetTranslationsOfOneTextIntoManyLanguagesCommand.cs

--- ConsoleClient/Models/GetTranslationsOfOneTextIntoManyLanguagesCommand.cs
+++ ConsoleClient/Models/GetTranslationsOfOneTextIntoManyLanguagesCommand.cs
@@ -2,7 +2,9 @@ namespace ConsoleClient.Models;
 
 public class GetTranslationsOfOneTextIntoManyLanguagesCommand
 {
+    [JsonProperty(Required = Required.Always, PropertyName = "text")]
     public required string Text { get; init; }
-    
+
+    [JsonProperty(Required = Required.Always, PropertyName = "languages")]
     public required IEnumerable<string> Languages { get; init; }
 }
