f1b4b39b784df682145afee83db7f4bd71976fdc
Алексей <97665299+0ne290@users.noreply.github.com>
2024-11-02 22:19:29 +0800

    Update Information.cs

--- ConsoleClient/Models/Information.cs
+++ ConsoleClient/Models/Information.cs
@@ -2,7 +2,9 @@ namespace ConsoleClient.Models;
 
 public class Information
 {
+    [JsonProperty(Required = Required.Always, PropertyName = "cacheProvider")]
     public required string CacheProvider { get; init; }
-    
+
+    [JsonProperty(Required = Required.Always, PropertyName = "server")]
     public required string Server { get; init; }
 }
