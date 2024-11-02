6584175cfb95ba68a5be5e9d40102140170b8d4f
Алексей <97665299+0ne290@users.noreply.github.com>
2024-11-02 22:11:25 +0800

    Update TranslatingManyTextsIntoOneLanguageResult.cs

--- Core/Dtos/TranslatingManyTextsIntoOneLanguageResult.cs
+++ Core/Dtos/TranslatingManyTextsIntoOneLanguageResult.cs
@@ -2,7 +2,9 @@ namespace Core.Dtos;
 
 public class TranslatingManyTextsIntoOneLanguageResult
 {
+    [JsonProperty(Required = Required.Always, PropertyName = "language")]
     public required string Language { get; init; }
 
+    [JsonProperty(Required = Required.Always, PropertyName = "translationsBySourceTexts")]
     public required IDictionary<string, string> TranslationsBySourceTexts { get; init; }
 }
